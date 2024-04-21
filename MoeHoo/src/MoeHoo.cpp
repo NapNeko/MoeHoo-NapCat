e32d143f783dfb5624d79e841288e4d30b475eac
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-21 21:35:28 +0800

    fix

--- MoeHoo/src/MoeHoo.cpp
+++ MoeHoo/src/MoeHoo.cpp
@@ -3,6 +3,34 @@
 #include <vector>
 #include <string>
 #include "MoeHoo.h"
+#include <node_api.h>
+//"${CMAKE_SOURCE_DIR}/node_modules/node-api-header/include"
+namespace demo {
+
+napi_value Method(napi_env env, napi_callback_info args) {
+  napi_value greeting;
+  napi_status status;
+
+  status = napi_create_string_utf8(env, "world", NAPI_AUTO_LENGTH, &greeting);
+  if (status != napi_ok) return nullptr;
+  return greeting;
+}
+
+napi_value init(napi_env env, napi_value exports) {
+  napi_status status;
+  napi_value fn;
+
+  status = napi_create_function(env, nullptr, 0, Method, nullptr, &fn);
+  if (status != napi_ok) return nullptr;
+
+  status = napi_set_named_property(env, exports, "hello", fn);
+  if (status != napi_ok) return nullptr;
+  return exports;
+}
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
+
+}  // namespace demo
 
 std::vector<char> ReadFileToMemory(const std::string &filePath)
 {
@@ -29,7 +57,7 @@ size_t SearchHexPattern(const std::vector<char> &data, const std::string &hexPat
 	return pos;
 }
 
-int main()
+int def_test()
 {
 	try
 	{
---
+++ Test/test.js
@@ -0,0 +1,1 @@
+require("./build/Debug/MoeHoo.node")
