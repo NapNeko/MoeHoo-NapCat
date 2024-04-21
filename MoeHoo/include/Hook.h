90ce91fc8d2a7a8b7da7df0f7a237159facf6dd2
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-21 22:03:26 +0800

    fix

--- MoeHoo/include/Hook.h
+++ MoeHoo/include/Hook.h
@@ -2,5 +2,24 @@
 
 class InlineHook
 {
-    
+
 };
+/*
+jmp xxxxx
+原理有问题 jmp后应该保证自动jmp回来 待实现 在hook阶段unhook 对于多线程比较灾难 可以参考Frida的HOOK
+void Hook()
+{
+	DWORD OldProtect;
+	if (VirtualProtect(OldMessageBoxW, 12, PAGE_EXECUTE_READWRITE, &OldProtect))
+	{
+		memcpy(Ori_Code, OldMessageBoxW, 12);               // 拷贝原始机器码指令
+		*(PINT64)(HookCode + 2) = (INT64)&MyMessageBoxW;    // 填充90为指定跳转地址
+	}
+	memcpy(OldMessageBoxW, &HookCode, sizeof(HookCode));    // 拷贝Hook机器指令
+}
+
+void UnHook()
+{
+	memcpy(OldMessageBoxW, &Ori_Code, sizeof(Ori_Code));    // 恢复hook原始代码
+}
+*/
