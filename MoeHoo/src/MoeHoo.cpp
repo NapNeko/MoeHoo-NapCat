ca3d74980bd05788dcbabb8d5c6714c4e44677bb
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-22 12:56:28 +0800

    fix

--- MoeHoo/src/MoeHoo.cpp
+++ MoeHoo/src/MoeHoo.cpp
@@ -19,33 +19,6 @@ DWORD_PTR searchRkeyDownloadHook()
 	DWORD_PTR address = SearchInModule(wrapperModule, hexPattern);
 	return address;
 }
-int def_test()
-{
-	try
-	{
-		// 读取PE文件到内存
-		std::vector<char> PEData(ReadFileToMemory("E:\\APPD\\NTQQ\\resources\\app\\versions\\9.9.9-22961\\wrapper.node"));
-		std::string hexPattern = "\xE8\x62\x01\x8F\xFE";
-		size_t patternPos = SearchHexPattern(PEData, hexPattern);
-
-		std::cout << "十六进制序列的文件偏移位置: " << std::hex << patternPos << std::endl;
-
-		// 计算RVA
-		// .text 代码节的RVA为0x1000，文件偏移为0x400
-		size_t codeSectionRVA = 0x1000;
-		size_t codeSectionFOA = 0x400;
-		size_t rva = codeSectionRVA + (patternPos - codeSectionFOA);
-
-		std::cout << "十六进制序列的RVA: " << std::hex << rva << std::endl;
-		std::cout << "十六进制序列的Hook点:" << std::hex << rva - 0x3 << std::endl;
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << "错误: " << e.what() << std::endl;
-	}
-	system("pause");
-	return 0;
-}
 
 namespace demo
 {
@@ -79,3 +52,31 @@ namespace demo
 	NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
 
 } 
+
+int def_test()
+{
+	try
+	{
+		// 读取PE文件到内存
+		std::vector<char> PEData(ReadFileToMemory("E:\\APPD\\NTQQ\\resources\\app\\versions\\9.9.9-22961\\wrapper.node"));
+		std::string hexPattern = "\xE8\x62\x01\x8F\xFE";
+		size_t patternPos = SearchHexPattern(PEData, hexPattern);
+		
+		std::cout << "十六进制序列的文件偏移位置: " << std::hex << patternPos << std::endl;
+
+		// 计算RVA
+		// .text 代码节的RVA为0x1000，文件偏移为0x400
+		size_t codeSectionRVA = 0x1000;
+		size_t codeSectionFOA = 0x400;
+		size_t rva = codeSectionRVA + (patternPos - codeSectionFOA);
+
+		std::cout << "十六进制序列的RVA: " << std::hex << rva << std::endl;
+		std::cout << "十六进制序列的Hook点:" << std::hex << rva - 0x3 << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "错误: " << e.what() << std::endl;
+	}
+	system("pause");
+	return 0;
+}
