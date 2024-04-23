ff78b0a4899bfa88d942c4d93389fb7dbdbfb176
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-23 22:47:16 +0800

    fix

--- MoeHoo/src/MoeHoo.cpp
+++ MoeHoo/src/MoeHoo.cpp
@@ -20,7 +20,7 @@ FuncPtr func;
 // 没有做多线程安全与回调 可能大问题
 INT64 recvRkey(INT64 a1, char **a2)
 {
-	//MessageBoxA(0, "", *a2, 0);
+	// MessageBoxA(0, "", *a2, 0);
 	recvRkeyLock.lock();
 	rkey = *a2;
 	INT64 ret = func(a1, a2);
@@ -58,7 +58,7 @@ INT64 searchRkeyDownloadHook()
 			done = true;
 			break;
 		}
-		//MessageBoxA(0, std::to_string(address).c_str(), std::to_string(searchOffset).c_str(), 0);
+		// MessageBoxA(0, std::to_string(address).c_str(), std::to_string(searchOffset).c_str(), 0);
 		hookorgptr = GetFunctionAddress(address);
 		BYTE *hookorgptr2 = reinterpret_cast<BYTE *>(hookorgptr);
 		// MessageBoxA(0, std::to_string(hookorgptr).c_str(), std::to_string(std::equal(expectedBytes.begin(), expectedBytes.end(), hookorgptr2)).c_str(), 0);
