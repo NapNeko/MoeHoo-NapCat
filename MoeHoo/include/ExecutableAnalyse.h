f640f6c9c1f6d18ec2632a93ec4f8365f8f455f9
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-24 10:54:58 +0800

    fix

--- MoeHoo/include/ExecutableAnalyse.h
+++ MoeHoo/include/ExecutableAnalyse.h
@@ -8,11 +8,10 @@
 #elif _LINUX_PLATFORM_
 #endif
 
-
 std::vector<char> ReadFileToMemory(const std::string &filePath);
 size_t SearchHexPattern(const std::vector<char> &data, const std::string &hexPattern);
 #ifdef _LINUX_PLATFORM_
-INT64 SearchRangeAddressInModule(void* module, const std::string& hexPattern, INT64 searchStartRVA, INT64 searchEndRVA);
+INT64 SearchRangeAddressInModule(void *module, const std::string &hexPattern, INT64 searchStartRVA = 0, INT64 searchEndRVA = 0);
 #elif _WIN_PLATFORM_
 INT64 SearchRangeAddressInModule(HMODULE module, const std::string &hexPattern, INT64 searchStartRVA = 0, INT64 searchEndRVA = 0);
 #endif
