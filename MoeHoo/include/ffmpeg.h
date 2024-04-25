2df381d7bc9e66ba1cb9b7b6d0812b4821d6526f
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-25 21:08:37 +0800

    fix

--- MoeHoo/include/ffmpeg.h
+++ MoeHoo/include/ffmpeg.h
@@ -1,10 +1,13 @@
 // 该部分用于替换FFMPEG依赖目标
+#include <windows.h>
 class ffmpeg
 {
 private:
 public:
-    static ffmpeg *GetInstance()
+    static ffmpeg *GetInstance(const char* Libary)
     {
+        //dlopen();
+        //LoadLibrary();
         return (ffmpeg *)nullptr;
     }
     bool InitFFmpeg()
