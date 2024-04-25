320ea57d172cc2cc6c40cd63ebbd117cfb31c853
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-25 20:16:39 +0800

    fix

--- MoeHoo/include/ffmpeg.h
+++ MoeHoo/include/ffmpeg.h
@@ -1,1 +1,21 @@
 // 该部分用于替换FFMPEG依赖目标
+class ffmpeg
+{
+private:
+public:
+    static ffmpeg *GetInstance()
+    {
+        return (ffmpeg *)nullptr;
+    }
+    bool InitFFmpeg()
+    {
+    }
+    void *Audio2Wav()
+    {
+        return nullptr;
+    }
+    void *VideoGetInfo()
+    {
+        return nullptr;
+    }
+};
